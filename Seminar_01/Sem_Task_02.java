d4db52ab547e3cc4fc630d7748eb9bd9b1fb6275
Alexey <mr.alexv2000@mail.ru>
2023-03-18 16:08:59 +0500

    Решение 2 задачи

---
+++ Seminar_01/Sem_Task_02.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.*;      
+
+
+public class Sem_Task_02 
+{
+    public static void main(String[] args) 
+    {
+        int[] array = new int[] {1, 1, 0, 1, 1, 1, 1, 0, 1};
+        int n = 0;
+        int max = 0;
+        for (int i = 0; i < array.length; i++) 
+        {
+            if (array[i] == 1)
+            {
+                n++;
+                if (n > max)
+                {
+                    max = n;
+                }
+            }
+            else
+            {
+                n = 0;
+            }
+        }
+        System.out.printf("maximum consecutive 1 = %s ", max);
+    }
+}
