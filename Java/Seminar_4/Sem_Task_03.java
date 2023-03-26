7e28e14e4fdf4be38228c4b2fd08be2c37eebd3a
Alexey <mr.alexv2000@mail.ru>
2023-03-26 23:58:32 +0500

    Решение 3 задачи

---
+++ Java/Seminar_4/Sem_Task_03.java
@@ -0,0 +1,37 @@
+// 1. Принимает от пользователя и “запоминает” строки.
+// 2. Если введено print, выводит строки так, чтобы последняя введенная
+// была первой в списке, а первая - последней.
+
+package Java.Seminar_4;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+//import javax.sql.rowset.serial.SQLOutputImpl;
+
+public class Sem_Task_03 
+{
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        Deque <String> queue = new LinkedList<String>();
+
+        System.out.println("Enter string: ");
+        String str = scn.nextLine();
+        while (! str.contains("print")) 
+        {
+            queue.add(str);
+            System.out.print(queue.toString());
+            System.out.println("Enter new string: ");
+            str = scn.nextLine();
+        }
+        while(! queue.isEmpty()) 
+        {
+            System.out.println(queue.pollLast());
+        }
+        System.out.print(queue.toString());
+        scn.close();
+    }
+}
+
