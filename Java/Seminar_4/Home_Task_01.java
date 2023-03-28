9795ccb3c9313d4eb9ce8b022b899c2b909bdd4e
Alexey <mr.alexv2000@mail.ru>
2023-03-28 23:06:03 +0500

    Решение 2 дз

--- Java/Seminar_4/Home_Task_01.java
+++ Java/Seminar_4/Home_Task_01.java
@@ -2,6 +2,27 @@
 
 package Java.Seminar_4;
 
-public class Home_Task_01 {
+import java.util.LinkedList;
+import java.util.Arrays;
+
+public class Home_Task_01
+{ 
+    public static void main(String[] args) 
+    {
+        LinkedList<Integer> arr = new LinkedList<>(Arrays.asList(1,2,3,4,5));
+        System.out.println("Начальный   массив: ");
+        System.out.println(arr);
+        System.out.println("Перевернутый  массив: ");
+        System.out.println(reverse(arr));
+    }
     
+    public static LinkedList<Integer> reverse(LinkedList<Integer> ls) 
+    {
+        LinkedList<Integer> rev_ls = new LinkedList<>();
+        for(int i=ls.size()-1;i>=0;i--)
+        {
+            rev_ls.add(ls.get(i));
+        }
+        return rev_ls;        
+    } 
 }
--- Java/Seminar_4/Home_Task_02.java
+++ Java/Seminar_4/Home_Task_02.java
@@ -1,10 +1,85 @@
 // Реализуйте очередь с помощью LinkedList со следующими методами:
-// enqueue() - помещает элемент в конец очереди,
+//      enqueue() - помещает элемент в конец очереди,
 // dequeue() - возвращает первый элемент из очереди и удаляет его,
 // first() - возвращает первый элемент из очереди, не удаляя.
 
 package Java.Seminar_4;
 
-public class Home_Task_02 {
-    
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Home_Task_02 
+{
+    static void interfaceik()
+    {
+        System.out.println("Введите 1, для помещения эллемента в конец очереди");
+        System.out.println("Введите 2, для возрата и удаления первого эллемента");
+        System.out.println("Введите 3, для возрата первого эллемента");
+        System.out.println("Введите stop для завершения работы программы");
+        System.out.print("Введите номер команды (из выше перечисленных): ");
+    }
+
+    public static void enqueue(LinkedList<String> list, String str) 
+    {
+        list.add(str);
+        System.out.println(list);
+    }
+
+    public static void dequeue(LinkedList<String> list) 
+    {
+        String temp = list.get(0);
+        list.remove(0);
+        System.out.println("Возвращаемое и удаленное значение: " + temp);
+    }
+
+    public static void first(LinkedList<String> list) 
+    {
+        String temp = list.get(0);
+        System.out.println("Возвращаемое значение: " + temp);
+    }
+
+    public static void main(String[] args) 
+    {
+        LinkedList<String> arr = new LinkedList<>();
+
+        for (int i = 0; i < 5; i++) 
+        {
+            arr.add(i, "Переменная " + (i + 1));
+        }
+
+        System.out.println("Начальный массив: ");
+        System.out.println(arr);
+
+        Scanner scan = new Scanner(System.in);
+        interfaceik();
+        String str = scan.nextLine();
+
+        while (!str.equals("stop")) 
+        {
+
+            if (str.equals("1"))
+            {
+                System.out.print("Введите новый эллемент для добавления: ");
+                String new_str = scan.nextLine();
+                enqueue(arr, new_str);
+                System.out.println("Эллемент добавлен в конец");
+            }
+            if (str.equals("2"))
+            {
+                dequeue(arr);
+                System.out.println("Измененный массив: ");
+                System.out.println(arr);
+            }
+            if (str.equals("3"))
+            {
+                first(arr);
+                System.out.println("Имеющийся  массив: ");
+                System.out.println(arr);
+            }
+            System.out.print("Введите номер команды (из выше перечисленных): ");
+            str = scan.nextLine();
+        
+        }
+        scan.close();
+    }
 }
